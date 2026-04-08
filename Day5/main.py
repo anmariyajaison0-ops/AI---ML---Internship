487e5389886c6f3eb8cfbc1304be28ef9f3bf3e0
Anmariya Jaison <anmariyajaison0@gmail.com>
2026-04-08 22:58:52 +0530

    Day 5 Completed

---
+++ Day5/main.py
@@ -0,0 +1,4 @@
+import mymodule
+
+print(mymodule.greet("Anmariya"))
+print(mymodule.square(5))
---
+++ Day5/mymodule.py
@@ -0,0 +1,5 @@
+def greet(name):
+    return "Hello " + name
+
+def square(num):
+    return num * num
