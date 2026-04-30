ab129af01a5c7218c6f9c36c9d25b0db6df8b578
Anmariya Jaison <anmariyajaison0@gmail.com>
2026-04-30 23:01:51 +0530

    Day 15 Completed

---
+++ Day15/task_2(day15).py
@@ -0,0 +1,13 @@
+marks = [80, 90, 70, 60, 50]
+
+# Average
+avg = sum(marks) / len(marks)
+
+# Max & Min
+maximum = max(marks)
+minimum = min(marks)
+
+# Count
+count = len(marks)
+
+print(avg, maximum, minimum, count)
